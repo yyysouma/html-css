c3672961e490083b99923e40a2a85668664f30c0
yyysouma <worka7652@gmail.com>
2024-12-17 23:26:13 +0900

    Add files via upload

---
+++ JavaScript/変数への代入とその確認方法/js/script.js
@@ -0,0 +1,31 @@
+//変数にNumber型を代入してconsole.log()で表示させてください。
+var variableNumber = 1234;
+console.log(variableNumber);
+
+//変数にString型を代入してconsole.log()で表示させてください。
+var variableText = "Text";
+console.log(variableText);
+
+//変数にBoolean型を代入してconsole.log()で表示させてください。
+var variableBoolean = true;
+console.log(variableBoolean);
+
+//10と20を足した結果を変数に代入してconsole.log()で表示させてください。
+var NumberPlus = 10 + 20;
+console.log(NumberPlus);
+
+//20から10を引いた結果を変数に代入してconsole.log()で表示させてください。
+var NumberMinus = 20 - 10;
+console.log(NumberMinus);
+
+//50へ2を掛けた結果を変数に代入してconsole.log()で表示させてください。
+var NumberBy = 50 * 2;
+console.log(NumberBy);
+
+//20を2で割った結果を変数に代入してconsole.log()で表示させてください。
+var NumberFraction = 20 / 2;
+console.log(NumberFraction);
+
+//12を34を結合させた結果を変数に代入してconsole.log()で表示させてください。
+var NumberCombination = "12" + "34";
+console.log(NumberCombination);
