b9df31e3a55a66d96eabd3df9f16ec5ebf5bf2ae
yyysouma <worka7652@gmail.com>
2024-12-19 22:38:09 +0900

    Add files via upload

---
+++ JavaScript/if文/js/script.js
@@ -0,0 +1,17 @@
+//1.変数にtrueを代入し、ifの条件がtrueの時にconsole.log();で変数の中身を表示させる処理を作ってください。
+var test = true;
+if (true == true){
+    console.log(test);
+}
+
+//2.変数に100を代入し、ifの条件がtrueの時にconsole.log();で変数の中身を表示させる処理を作ってください。
+var num = 100;
+if (num === 100){
+    console.log(num);
+}
+
+//3.変数に"JavaScript"を代入し、ifの条件がtrueの時にconsole.log();で変数の中身を表示させる処理を作ってください。
+var text = "JavaScript";
+if (text == "JavaScript"){
+    console.log(text);
+}
