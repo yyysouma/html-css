c89c92ec591d41c34bf52099172c786048a1c604
yyysouma <worka7652@gmail.com>
2024-12-23 22:04:30 +0900

    Add files via upload

---
+++ JavaScript/関係演算子/js/script.js
@@ -0,0 +1,27 @@
+//xがyより大きい時にifの処理へ入るようにしてください。
+const x = 5;
+const y = 3;
+if (x > y) {
+    console.log('xはyより大きい');
+}
+
+//xがyより小さい時にifの処理へ入るようにしてください。
+const x = 3;
+const y = 5;
+if (x < y) {
+    console.log('xはyより小さい');
+}
+
+//xがy以上の時にifの処理へ入るようにしてください。
+const x = 5;
+const y = 3;
+if (x >= y) {
+    console.log('xはy以上');
+}
+
+//xがy以下の時にifの処理へ入るようにしてください。
+const x = 3;
+const y = 5;
+if (x <= y) {
+    console.log('xはy以下');
+}
