78152aaf85179823f817460ce819b757f9b5d5fb
yyysouma <worka7652@gmail.com>
2024-12-24 20:46:21 +0900

    Add files via upload

---
+++ JavaScript/三項演算子/js/script.js
@@ -0,0 +1,23 @@
+//変数の中にtrueまたは、falseが代入された時の処理を作ってください。
+const true = 
+const result = 3 >= 2 ? true : false;
+
+//代入された変数の中が5より大きい時にtrueの処理と、5より小さい時にfalseの処理を作ってください。
+const age = 6;
+const age2 = age > 5 ? 'trueの処理' : 'falseの処理';
+console.log(age2);
+
+//代入された変数の中が5より小さい時にtrueの処理と、5より大きい時にfalseの処理を作ってください。
+const age3 = 6;
+const age4 = age3 < 5 ? 'trueの処理' : 'falseの処理';
+console.log(age4);
+
+//代入された変数の中が10以上の時にtrueの処理と、10以下の時にfalseの処理を作ってください。
+const age5 = 10;
+const age6 = age5 >= 10 ? 'trueの処理' : 'falseの処理';
+console.log(age6);
+
+//代入された変数の中に値が存在する場合はtrue、存在しない場合はfalseの処理にしてください。
+const age7 = 10;
+const age8 = age7 == 10 ? 'trueの処理' : 'falseの処理';
+console.log(age8);
