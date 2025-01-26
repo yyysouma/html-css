d80f261dc749c462954534d3e336bd5ff184e90a
yyysouma <worka7652@gmail.com>
2025-01-26 17:11:17 +0900

    Add files via upload

--- JavaScript/論理演算子/js/script.js
+++ JavaScript/論理演算子/js/script.js
@@ -11,20 +11,20 @@ if (age >= 20 && age <= 29){
 /*OR(||)の条件を作成し、その処理に入るよう作ってください。
 (elseの処理も作成してください)
 */
-const age2 = 65;
-if (age2 <= 18 || age2 <= 60){
-    console.log('成人');
-} else {
+const age2 = 25;
+if (age2 <= 18 || age2 >= 60){
     console.log('ジュニアまたはシニア');
+} else {
+    console.log('成人');
 }
 
 /*NOT(!)の条件を作成し、その処理に入るよう作ってください。
 比較条件は数値型の比較でお願いします。
 (elseの処理も作成してください)
 */
-var boolean = 90;
-if (!boolean <= 80 || !boolean >= 100) {
-    console.log('規格内');    
+var boolean = 81;
+if (!(boolean <= 80)) {
+    console.log('規格外');    
 } else {
-    console.log('規格外');
+    console.log('規格内');
 }
